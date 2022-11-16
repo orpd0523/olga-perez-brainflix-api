67f57fa95b8ca87d9b86a06d6031f055305c1a63
orpd0523 <orpd0523@gmail.com>
2022-11-16 15:40:55 -0600

    configured cors, dotenv, data folder, static files

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-const { response } = require('express');
 const express = require('express');
+const cors = require('cors')
 const app = express();
+const fs = require("fs");
+require('dotenv').config();
+app.use(cors())
+app.use(express.json());
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
     res.send("Welcome to /")
-}).get("/videos", function(req, res){
+})
+
+app.get("/videos", function(req, res){
     res.send("Welcome to /videos")
-}).get("/videos/:id", function(req, res){
+})
+
+app.get("/videos/:id", function(req, res){
     res.send("Welcome to /videos/:id")
-}).post("/videos", function(req, res){
+})
+
+app.post("/videos", function(req, res){
     res.send("Welcome to /videos")
 })
 
 app.listen(3000, function(){
     console.log("Hello World")
 })
+
+module.exports = app;
