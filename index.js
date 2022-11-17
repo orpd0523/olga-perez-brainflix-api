ece25e7c13ae1b85fa344857acde3f611cb1a4bb
orpd0523 <orpd0523@gmail.com>
2022-11-16 21:16:16 -0600

    created logic for first end point

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
-const express = require('express');
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const fs = require("fs");
-require('dotenv').config();
-app.use(cors())
+require("dotenv").config();
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
-    res.send("Welcome to /")
-})
+app.get("/", function (req, res) {
+  res.send("Welcome to /");
+});
 
-app.get("/videos", function(req, res){
-    res.send("Welcome to /videos")
-})
+app.get("/videos", function (req, res) {
+  const videoDetailsJSON = fs.readFileSync("./data/video-details.json");
+  let videoDetails = JSON.parse(videoDetailsJSON);
+  let results = videoDetails.map((video) => {
+    return {
+      id: video.id,
+      title: video.title,
+      channel: video.channel,
+      image: video.image
+    };
+  });
+  res.send(results);
+});
 
-app.get("/videos/:id", function(req, res){
-    res.send("Welcome to /videos/:id")
-})
+app.get("/videos/:id", function (req, res) {
+  res.send("Welcome to /videos/:id");
+});
 
-app.post("/videos", function(req, res){
-    res.send("Welcome to /videos")
-})
+app.post("/videos", function (req, res) {
+  res.send("Welcome to /videos");
+});
 
-app.listen(3000, function(){
-    console.log("Hello World")
-})
+app.listen(3000, function () {
+  console.log("Hello World");
+});
 
 module.exports = app;
