4781d7681266298559ffb226f546510cc5a092b4
orpd0523 <orpd0523@gmail.com>
2022-11-16 22:25:14 -0600

    final endpoint added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const crypto = require("crypto");
 const cors = require("cors");
 const app = express();
 const fs = require("fs");
@@ -32,7 +33,29 @@ app.get("/videos/:id", function (req, res) {
 });
 
 app.post("/videos", function (req, res) {
-  res.send("Welcome to /videos");
+  const body = req.body;
+  const video = {
+    id: crypto.randomUUID(),
+    title: body.title,
+    channel: "Molotov",
+    image: "https://i.scdn.co/image/ab67616d0000b2735e2b069b2306267d474c43dd",
+    description: body.description,
+    views: "0",
+    likes: "0",
+    duration: "6:66",
+    video: "https://project-2-api.herokuapp.com/stream",
+    timestamp: Date.now(),
+    comments: [],
+  };
+  const videoDetailsJSON = fs.readFileSync("./data/video-details.json");
+  const videoDetails = JSON.parse(videoDetailsJSON);
+  const results = [...videoDetails, video];
+  fs.writeFile('./data/video-details.json', JSON.stringify(results), err => {
+    if (err) {
+      console.error(err);
+    }
+  });
+  res.send(results);
 });
 
 app.listen(3000, function () {
